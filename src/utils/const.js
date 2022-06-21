488bab8b8ad12c42cec754314acc16ffaab550ce
MorningGQC <32951366+MorningGQC@users.noreply.github.com>
2022-06-22 03:19:47 +0800

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -20,15 +20,15 @@ const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength, year) => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
-  return `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据`;
+  return `从第一次用 APP 记录跑步到现在 ${yearLength} 年了，这是${yearStr}跑过的路`;
 }
 const ENGLISH_INFO_MESSAGE = (yearLength, year) =>
   `Running Journey with ${yearLength} Years, the table shows year ${year} data`;
 
 // not support English for now
 const CHINESE_LOCATION_INFO_MESSAGE_FIRST =
-  '我跑过了一些地方，希望随着时间推移，地图点亮的地方越来越多';
-const CHINESE_LOCATION_INFO_MESSAGE_SECOND = '不要停下来，不要停下奔跑的脚步';
+  '我希望能越跑越远';
+const CHINESE_LOCATION_INFO_MESSAGE_SECOND = 'Ceaca Trova';
 
 const INFO_MESSAGE = IS_CHINESE ? CHINESE_INFO_MESSAGE : ENGLISH_INFO_MESSAGE;
 const FULL_MARATHON_RUN_TITLE = IS_CHINESE ? '全程马拉松' : 'Full Marathon';
