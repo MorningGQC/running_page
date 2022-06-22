add594f0c235abfdd1e84d4a47940dac773793f0
MorningGQC <32951366+MorningGQC@users.noreply.github.com>
2022-06-22 09:31:54 +0800

    Update index.jsx

--- src/components/LocationStat/index.jsx
+++ src/components/LocationStat/index.jsx
@@ -3,6 +3,7 @@ import YearStat from 'src/components/YearStat';
 import {
   CHINESE_LOCATION_INFO_MESSAGE_FIRST,
   CHINESE_LOCATION_INFO_MESSAGE_SECOND,
+  CHINESE_LOCATION_INFO_MESSAGE_THIRD,
 } from '../../utils/const';
 import CitiesStat from './CitiesStat';
 import LocationSummary from './LocationSummary';
@@ -13,13 +14,14 @@ const LocationStat = ({ changeYear, changeCity, changeTitle }) => (
     <section className="pb4" style={{ paddingBottom: '0rem' }}>
       <p style={{ lineHeight: 1.8 }}>
         {CHINESE_LOCATION_INFO_MESSAGE_FIRST}
-        .
+        //.
         <br />
         {CHINESE_LOCATION_INFO_MESSAGE_SECOND}
-        .
+        //.
         <br />
+        {CHINESE_LOCATION_INFO_MESSAGE_THIRD}
         <br />
-        Yesterday you said tomorrow.
+        Cerca Trova//.
       </p>
     </section>
     <hr color="red" />
